fa5f7d45271d453ab438b8fd07c5a834fc1e8217
J-P Nurmi <jpnurmi@gmail.com>
2020-06-14 19:45:40 +0200

    Rename & comment out things

--- lib/assimp.dart
+++ lib/assimp.dart
@@ -2,9 +2,7 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-/// A synchronous SQLite wrapper.
-///
-/// Written using dart:ffi.
-library sqlite;
+/// Dart bindings for Assimp
+library assimp;
 
-export "src/database.dart";
+export 'src/scene.dart';
