56216a201391b2f1db5e25b9a4bce632ff079057
J-P Nurmi <jpnurmi@gmail.com>
2020-06-16 17:47:53 +0200

    Scene: add tests

---
+++ test/scene_test.dart
@@ -0,0 +1,137 @@
+/*
+---------------------------------------------------------------------------
+Open Asset Import Library (assimp)
+---------------------------------------------------------------------------
+
+Copyright (c) 2006-2020, assimp team
+
+All rights reserved.
+
+Redistribution and use of this software in source and binary forms,
+with or without modification, are permitted provided that the following
+conditions are met:
+
+* Redistributions of source code must retain the above
+copyright notice, this list of conditions and the
+following disclaimer.
+
+* Redistributions in binary form must reproduce the above
+copyright notice, this list of conditions and the
+following disclaimer in the documentation and/or other
+materials provided with the distribution.
+
+* Neither the name of the assimp team, nor the names of its
+contributors may be used to endorse or promote products
+derived from this software without specific prior
+written permission of the assimp team.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+---------------------------------------------------------------------------
+*/
+
+import 'dart:io';
+import 'package:path/path.dart' hide equals;
+import 'package:test/test.dart';
+import 'package:assimp/assimp.dart';
+
+String testPath(String fileName) => 'test/models/model-db/' + fileName;
+
+const Matcher isNullPointer = _IsNullPointer();
+
+class _IsNullPointer extends Matcher {
+  const _IsNullPointer();
+  @override
+  bool matches(ptr, Map matchState) => ptr.isNull;
+  @override
+  Description describe(Description description) => description.add('nullptr');
+}
+
+void main() {
+  test('null', () {
+    Scene scene = Scene.fromNative(null);
+    expect(scene.flags, isZero);
+    expect(scene.rootNode, isNullPointer);
+    expect(scene.meshes, isEmpty);
+    expect(scene.materials, isEmpty);
+    expect(scene.animations, isEmpty);
+    expect(scene.textures, isEmpty);
+    expect(scene.lights, isEmpty);
+    expect(scene.cameras, isEmpty);
+    expect(scene.metaData, isNullPointer);
+  });
+
+  void testScene(String fileName, void tester(Scene scene)) {
+    final filePath = testPath(fileName);
+    final scene1 = Scene.fromFile(filePath);
+    tester(scene1);
+    scene1.dispose();
+
+//    final buffer = File(filePath).readAsStringSync();
+//    final scene2 = Scene.fromBuffer(buffer, hint: extension(filePath));
+//    tester(scene2);
+//    scene2.dispose();
+  }
+
+  test('3mf', () {
+    testScene('3mf/box.3mf', (scene) {
+      expect(scene.flags, isZero);
+      expect(scene.rootNode, isNotNull);
+      expect(scene.meshes.length, equals(1));
+      expect(scene.materials.length, equals(1));
+      expect(scene.animations.length, isZero);
+      expect(scene.textures.length, isZero);
+      expect(scene.lights.length, isZero);
+      expect(scene.cameras.length, isZero);
+      expect(scene.metaData, isNullPointer);
+    });
+    testScene('3mf/spider.3mf', (scene) {
+      expect(scene.flags, isZero);
+      expect(scene.rootNode, isNotNull);
+      expect(scene.meshes.length, equals(19));
+      expect(scene.materials.length, equals(4));
+      expect(scene.animations.length, isZero);
+      expect(scene.textures.length, isZero);
+      expect(scene.lights.length, isZero);
+      expect(scene.cameras.length, isZero);
+      expect(scene.metaData, isNullPointer);
+    });
+  });
+
+  test('fbx', () {
+    testScene('fbx/huesitos.fbx', (scene) {
+      expect(scene.flags, isZero);
+      expect(scene.rootNode, isNotNull);
+      expect(scene.meshes.length, equals(1));
+      expect(scene.materials.length, equals(1));
+      expect(scene.animations.length, equals(1));
+      expect(scene.textures.length, isZero);
+      expect(scene.lights.length, equals(1));
+      expect(scene.cameras.length, equals(1));
+      expect(scene.metaData, isNotNull);
+    });
+  });
+
+  test('obj', () {
+    testScene('Obj/Spider/spider.obj', (scene) {
+      expect(scene.flags, isZero);
+      expect(scene.rootNode, isNotNull);
+      expect(scene.meshes.length, equals(19));
+      expect(scene.materials.length, equals(6));
+      expect(scene.animations.length, isZero);
+      expect(scene.textures.length, isZero);
+      expect(scene.lights.length, isZero);
+      expect(scene.cameras.length, isZero);
+      expect(scene.metaData, isNullPointer);
+    });
+  });
+}
