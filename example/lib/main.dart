8b56212de30cce57485b5b8b974171e807133e93
J-P Nurmi <jpnurmi@gmail.com>
2020-07-10 17:28:46 +0200

    Example: add support for points and lines

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,7 +4,7 @@ import 'dart:typed_data';
 import 'dart:ui';
 
 import 'package:assimp/assimp.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Matrix4;
 import 'package:flutter/painting.dart';
 import 'package:flutter/services.dart';
 import 'package:path/path.dart';
@@ -155,6 +155,7 @@ class ScenePainter extends CustomPainter {
   final Scene scene;
   final Aabb3 bounds;
   final double scale;
+  final Color color = Colors.grey;
 
   ScenePainter(this.scene, this.bounds, this.scale);
 
@@ -162,8 +163,10 @@ class ScenePainter extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     if (scene == null) return;
 
-    final color = Colors.white;
-    final light = Vector3(0.0, 0.0, bounds.max.z / scale);
+    final paint = Paint();
+    paint.color = color;
+
+    final light = Vector3(0.0, 0.0, bounds.max.z * 2 / scale);
     final extent = bounds.max.distanceTo(bounds.min);
     final transformation = scene.rootNode.transformation;
 
@@ -172,55 +175,101 @@ class ScenePainter extends CustomPainter {
       ..scale(size.shortestSide / extent * scale);
 
     for (final mesh in scene.meshes) {
-      final normals = mesh.normalData;
-      final vertices = mesh.vertexData;
-
-      double faceDepth(Uint32List indices, Float32List vertices) {
-        return (vertices[indices[0] * 3 + 2] +
-                vertices[indices[1] * 3 + 2] +
-                vertices[indices[2] * 3 + 2]) /
-            3;
-      }
-
-      final faces = SplayTreeMap<double, Face>.fromIterable(mesh.faces,
-          key: (f) => faceDepth(f.indexData, vertices), value: (f) => f);
-
-      final count = faces.length * 3;
-      final colors = Int32List(count);
-      final indices = Uint16List(count);
-      final positions = Float32List(count * 2);
-
-      var i = 0;
-      for (final face in faces.values) {
-        for (final j in face.indices) {
-          final c = light.dot(matrix.transformed3(normals.vectorAt(j * 3)));
-          if (c < 0 || c.isNaN) continue;
-
-          colors[i] = Color.fromARGB(
-            255,
-            (color.red / 255 * c).round(),
-            (color.green / 255 * c).round(),
-            (color.blue / 255 * c).round(),
-          ).value;
-
-          final v = matrix.transformed3(vertices.vectorAt(j * 3));
-          positions[i * 2] = v.x;
-          positions[i * 2 + 1] = v.y;
-
-          indices[i] = i++;
-        }
-      }
-
-      if (i != 0) {
-        final raw = Vertices.raw(
-          VertexMode.triangles,
-          positions.sublist(0, i * 2),
-          colors: colors.sublist(0, i),
-          indices: indices.sublist(0, i),
-        );
-        canvas.drawVertices(raw, BlendMode.src, Paint());
+      switch (mesh.primitiveTypes) {
+        case PrimitiveType.point:
+          canvas.drawRawPoints(PointMode.points, points(mesh, matrix), paint);
+          break;
+        case PrimitiveType.line:
+          canvas.drawRawPoints(PointMode.lines, lines(mesh, matrix), paint);
+          break;
+        case PrimitiveType.triangle:
+          canvas.drawVertices(
+              vertices(mesh, matrix, light), BlendMode.src, paint);
+          break;
+        default:
+          break;
       }
     }
+  }
+
+  Float32List points(Mesh mesh, Matrix4 matrix) {
+    final vertices = mesh.vertexData;
+    final points = Float32List(mesh.faces.length * 2);
+    var i = -1;
+    for (final face in mesh.faces) {
+      assert(face.indices.length == 1);
+      final j = face.indices.first;
+      final v = matrix.transformed3(vertices.vectorAt(j * 3));
+      points[++i] = v.x;
+      points[++i] = v.y;
+    }
+    return points;
+  }
+
+  Float32List lines(Mesh mesh, Matrix4 matrix) {
+    final vertices = mesh.vertexData;
+    final points = Float32List(mesh.faces.length * 4);
+    var i = -1;
+    for (final face in mesh.faces) {
+      assert(face.indices.length == 2);
+      for (final j in face.indices) {
+        final v = matrix.transformed3(vertices.vectorAt(j * 3));
+        points[++i] = v.x;
+        points[++i] = v.y;
+      }
+    }
+    return points;
+  }
+
+  Vertices vertices(Mesh mesh, Matrix4 matrix, Vector3 light) {
+    final normals = mesh.normalData;
+    final vertices = mesh.vertexData;
+
+    double faceDepth(Uint32List indices, Float32List vertices) {
+      return (vertices[indices[0] * 3 + 2] +
+              vertices[indices[1] * 3 + 2] +
+              vertices[indices[2] * 3 + 2]) /
+          3;
+    }
+
+    final faces = SplayTreeMap<double, Face>.fromIterable(mesh.faces,
+        key: (f) => faceDepth(f.indexData, vertices), value: (f) => f);
+
+    final count = faces.length * 3;
+    final colors = Int32List(count);
+    final indices = Uint16List(count);
+    final positions = Float32List(count * 3);
+
+    var i = 0;
+    for (final face in faces.values) {
+      assert(face.indices.length == 3);
+      for (final j in face.indices) {
+        final c = light.dot(matrix.transformed3(normals.vectorAt(j * 3)));
+        if (c < 0 || c.isNaN) continue;
+
+        colors[i] = Color.fromARGB(
+          255,
+          (color.red / 255 * c).round(),
+          (color.green / 255 * c).round(),
+          (color.blue / 255 * c).round(),
+        ).value;
+
+        final v = matrix.transformed3(vertices.vectorAt(j * 3));
+        positions[i * 2] = v.x;
+        positions[i * 2 + 1] = v.y;
+
+        indices[i] = i++;
+      }
+    }
+
+    if (i == 0) return null;
+
+    return Vertices.raw(
+      VertexMode.triangles,
+      positions.sublist(0, i * 2),
+      colors: colors.sublist(0, i),
+      indices: indices.sublist(0, i),
+    );
   }
 
   @override
